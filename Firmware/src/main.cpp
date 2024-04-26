5d3f36fefb9c88edfd095687213ce7e04caaac0d
JLatex <jlatex@gmail.com>
2024-04-26 08:18:23 +0100

    Add a title and make the scrolling bit smaller

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -86,17 +86,31 @@ void RotaryEncoderChanged(bool clockwise, int id) {
 }
 
 M5Canvas canvas(&CoreS3.Display);
+M5Canvas titlecanvas(&CoreS3.Display);
+
+int canvasy;
 
 void setup() {
   auto cfg = M5.config();
   cfg.serial_baudrate = 115200;
   CoreS3.begin(cfg);
+
+  titlecanvas.createSprite(CoreS3.Display.width(), 40);
+  titlecanvas.setColorDepth(8);
+  titlecanvas.setTextColor(WHITE,BLUE);
+  titlecanvas.setTextSize(2);
+  titlecanvas.fillSprite(BLUE);
+  titlecanvas.println("M5 Remote Tester");
+  titlecanvas.pushSprite(0,0);
+  canvasy = titlecanvas.fontHeight()*2;
+
   canvas.setColorDepth(1);
-  canvas.createSprite(CoreS3.Display.width(), CoreS3.Display.height());
+  canvas.createSprite(CoreS3.Display.width(), CoreS3.Display.height()-canvasy);
   canvas.setPaletteColor(1, GREEN);
   canvas.setTextSize((float)canvas.width() / 160);
   canvas.setTextScroll(true);
-  canvas.pushSprite(0,0);
+  canvas.pushSprite(0,canvasy);
+  
   
   //initialize semaphore for reader task
   rotaryISRSemaphore = xSemaphoreCreateBinary();
@@ -224,5 +238,5 @@ void printf_log(const char *format, ...) {
     va_end(args);
     Serial.print(buf);
     canvas.printf(buf);
-    canvas.pushSprite(0, 0);
+    canvas.pushSprite(0, canvasy);
 }
